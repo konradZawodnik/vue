e43e8fe52cd6a177b289b197eb15c593c6ccd417
Konrad Zawodnik <konrad.zawodnik@wp.pl>
2023-07-04 15:57:52 +0200

    Simple example of form in vue.js

---
+++ vue-form/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <SignOffForm />
+</template>
+
+<script>
+import SignOffForm from './components/SignOffForm.vue'
+export default {
+  name: 'App',
+  components: {
+    SignOffForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
---
+++ vue-form/src/components/SignOffForm.vue
@@ -0,0 +1,144 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Email</label>
+    <input type="email" required v-model="email" />
+
+    <label>Password</label>
+    <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Frontend developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <label>Skills</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms" />
+      <label>Accept terms for conditions</label>
+    </div>
+
+    <div class="submit">
+      <button type="submit">Create an account</button>
+    </div>
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Skills: {{ skills }}</p>
+  <p>Terms accepted: {{ terms }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "",
+      terms: false,
+      tempSkill: "",
+      skills: [],
+      passwordError: "",
+    };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill) && !this.skills.includes(this.tempSkill.replaceAll(',', ''))) {
+          this.skills.push(this.tempSkill.replaceAll(',', ''));
+        }
+        this.tempSkill = "";
+      }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return skill !== item;
+      });
+    },
+    handleSubmit() {
+      if (this.password.length < 6) {
+        this.passwordError = "Password must have at least six characters";
+      }
+      if (this.password.length >= 6) {
+        this.passwordError = "";
+        alert("Form submitted");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  background: #0b6dff;
+  border: 0;
+  cursor: pointer;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+form {
+  max-width: 420px;
+  margin: 10px auto;
+  background: #fff;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #eee;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+.submit {
+  text-align: center;
+}
+</style>
